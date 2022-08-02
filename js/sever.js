9541c361d8b5e5cefed08aa9955d6e2c7539234b
UserTarun <saikalyantarun2001@gmail.com>
2022-08-02 13:41:28 +0530

    starting with backend for projects page

--- js/sever.js
+++ js/sever.js
@@ -1,4 +1,7 @@
 const express = require('express');
 const app = express();
+const MongoClient = require('mongodb').MongoClient;
+const PORT = 2121;
+require('dotenv').config();
 const cors = require('cors');
 
